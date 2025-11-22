d78c2feae0dcb488ac0ab0d7d7342c83226ad163
Tom Kintzel <kintzel@ecommerce-werkstatt.de>
2025-11-22 10:26:41 +0100

    add alternative to no events and partners

--- frontend/app/pages/index.vue
+++ frontend/app/pages/index.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="w-full">
         <!-- Hero Section -->
-         <!-- add strapi url to style background image -->
+        <!-- add strapi url to style background image -->
         <section class="w-full h-[70vh] flex items-center justify-center bg-cover bg-center bg-no-repeat px-6"
             :style="{ backgroundImage: `url(${imageUrl})` }">
             <div class="bg-white/80 p-8 rounded-2xl max-w-xl text-center shadow-lg">
@@ -13,55 +13,40 @@
                 </NuxtLink>
             </div>
         </section>
-        <Textsection
-            :title="homepage?.about?.title"
-            :text="homepage?.about?.text"
-        />
-
-        
-
+        <Textsection :title="homepage?.about?.title" :text="homepage?.about?.text" />
         <!-- Events Section -->
         <section class="px-6 py-16 bg-gray-100">
             <div class="max-w-6xl mx-auto">
                 <h3 class="text-3xl font-bold text-center mb-12">{{ homepage?.eventssection?.title }}</h3>
-
-                <div class="grid grid-cols-1 md:grid-cols-3 gap-8">
-                    <Eventitem
-                        v-for="event in homepage?.eventssection?.events"
-                        :key="event.id"
-                        :event="event"
-                    />
-                    <!-- <a  class="p-6 bg-white shadow rounded-2xl">
-                        <h4 class="text-xl font-bold mb-2">Sommerturnier 2025</h4>
-                        <p class="text-sm text-gray-600">15. Juni 2025</p>
-                    </a>
-                    <a class="p-6 bg-white shadow rounded-2xl">
-                        <h4 class="text-xl font-bold mb-2">Kids Camp</h4>
-                        <p class="text-sm text-gray-600">10.–14. Juli 2025</p>
-                    </a>
-                    <div class="p-6 bg-white shadow rounded-2xl">
-                        <h4 class="text-xl font-bold mb-2">Mixed Doppel Cup</h4>
-                        <p class="text-sm text-gray-600">August 2025</p>
-                    </div> -->
+                <div class="grid grid-cols-1 md:grid-cols-3 gap-8" v-if="homepage?.eventssection?.events">
+                    <Eventitem v-for="event in homepage?.eventssection?.events" :key="event.id" :event="event" />
+                </div>
+                <div class="grid grid-cols-1">
+                    <div class="bg-white shadow rounded-2xl p-6 mt-8 text-center">
+                        <p>Zur Zeit keine Events geplant</p>
+                    </div>
                 </div>
             </div>
         </section>
         <!-- Partners Section -->
         <section id="partners" class="px-6 py-16 bg-white">
             <div class="max-w-4xl mx-auto text-center">
-                <h3 class="text-3xl font-bold mb-4">{{homepage?.partners?.title}}</h3>
-                <p class="text-lg text-gray-700 mb-8">{{homepage?.partners?.text}}</p>
+                <h3 class="text-3xl font-bold mb-4">{{ homepage?.partners?.title }}</h3>
+                <p class="text-lg text-gray-700 mb-8">{{ homepage?.partners?.text }}</p>
             </div>
-            <div class="max-w-6xl mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
-                <Partneritem
-                    v-for="partner in homepage?.partners?.partners"
-                    :key="partner.id"
-                    :partner="partner"
-                />
-                <!-- <div class="flex items-center justify-center h-24 bg-slate-50 rounded-2xl shadow transition hover:shadow-lg text-xl font-semibold text-gray-800">ASICS</div>
-                <div class="flex items-center justify-center h-24 bg-slate-50 rounded-2xl shadow transition hover:shadow-lg text-xl font-semibold text-gray-800">Wilson</div>
-                <div class="flex items-center justify-center h-24 bg-slate-50 rounded-2xl shadow transition hover:shadow-lg text-xl font-semibold text-gray-800">Tennis Point</div> -->
-                <!-- <div class="flex items-center justify-center h-24 bg-slate-50 rounded-2xl shadow transition hover:shadow-lg text-xl font-semibold text-gray-800">Local Sponsor</div> -->
+            <div class="max-w-6xl mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8"
+                v-if="homepage?.partners?.partners">
+                <Partneritem v-for="partner in homepage?.partners?.partners" :key="partner.id" :partner="partner" />
+            </div>
+            <div class="max-w-6xl mx-auto grid-cols-1">
+                <div
+                    class="flex flex-col items-center justify-center bg-slate-50 rounded-2xl shadow transition hover:shadow-lg p-8">
+                    <span class="text-xl font-semibold text-gray-800">
+                        <NuxtLink to="/contact" class="text-ltu-blue hover:underline">
+                            Jetzt Partner werden!
+                        </NuxtLink>
+                    </span>
+                </div>
             </div>
         </section>
     </div>

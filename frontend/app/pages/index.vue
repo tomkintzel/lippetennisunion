48bed3c42926f8b46d7489dbc0c03d7dfd34d6e4
Tom Kintzel <kintzel@ecommerce-werkstatt.de>
2025-11-23 10:15:21 +0100

    add alternative to no events and partners

--- frontend/app/pages/index.vue
+++ frontend/app/pages/index.vue
@@ -18,10 +18,10 @@
         <section class="px-6 py-16 bg-gray-100">
             <div class="max-w-6xl mx-auto">
                 <h3 class="text-3xl font-bold text-center mb-12">{{ homepage?.eventssection?.title }}</h3>
-                <div class="grid grid-cols-1 md:grid-cols-3 gap-8" v-if="homepage?.eventssection?.events.length > 0">
+                <div class="grid grid-cols-1 md:grid-cols-3 gap-8" v-if="homepage?.eventssection?.events && homepage.eventssection.events.length > 0">
                     <Eventitem v-for="event in homepage?.eventssection?.events" :key="event.id" :event="event" />
                 </div>
-                <div class="grid grid-cols-1 max-w-xl place-items-center" v-else>
+                <div class="grid grid-cols-1 max-w-xl place-items-center mx-auto" v-else>
                     <div class="bg-white shadow rounded-2xl p-6 mt-8 text-center">
                         <p>Zur Zeit keine Events geplant</p>
                     </div>
